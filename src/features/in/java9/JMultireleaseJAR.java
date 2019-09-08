099f606bc71218e3f469c367ceb71b6a320ff9ab
ASatklichov <asatklichov@inwk.com>
2019-09-08 21:56:45 +0200

    edited

--- src/features/in/java9/JMultireleaseJAR.java
+++ src/features/in/java9/JMultireleaseJAR.java
@@ -7,7 +7,9 @@ package features.in.java9;
 *
  * In java 9, a new feature is introduced where a jar format has been enhanced
  * to have different versions of java class or resources can be maintained and
- * used as per the platform. In JAR, a file MANIFEST.MF file has a entry
+ * used as per the platform.
+
+In JAR, a file MANIFEST.MF file has a entry
  * Multi-Release: true in its main section. META-INF directory also contains a
  * versions subdirectory whose subdirectories (starting with 9 for Java 9 )
  * store version-specific classes and resource files.
