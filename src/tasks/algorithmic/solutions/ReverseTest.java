43ed71279a8acc0dd308b0431a5b457a649fe003
ASatklichov <asatklichov@inwk.com>
2019-12-13 14:17:13 +0100

    reverse algorithm tests

--- src/tasks/algorithmic/solutions/ReverseTest.java
+++ src/tasks/algorithmic/solutions/ReverseTest.java
@@ -13,9 +13,9 @@ package tasks.algorithmic.solutions;
  * </pre>
  * 
  */
-public class NumberReverse {
+public class ReverseTest {
 
-	static int reverse(int n) {
+	static int reverseNumber(int n) {
 		int rev = 0;
 		while (n != 0) {
 			rev = rev * 10 + (n % 10);
@@ -24,8 +24,54 @@ public class NumberReverse {
 		return rev;
 	}
 
+	/**
+	 * Reverse array only using in-place algorithm.
+	 * 
+	 * No allowed to use new array, or Java build-in methods
+	 * 
+	 * @param n
+	 * @return
+	 */
+	static char[] reverseCharArr(char[] arr) {
+		int len = arr.length;
+		boolean hasMidElement = len % 2 != 0;
+		for (int i = 0; i < len / 2; i++) {
+			if (hasMidElement || i == len / 2) {
+				swap(arr, len, i);
+				break;
+			}
+			swap(arr, len, i);
+		}
+
+		return arr;
+	}
+
+	private static void swap(char[] arr, int len, int i) {
+		char tmp = arr[len - i - 1];
+		arr[len - i - 1] = arr[i];
+		arr[i] = tmp;
+	}
+
 	public static void main(String a[]) {
 
-		System.out.println(reverse(143303));
+		int num = 143303;
+		System.out.println(num);
+		System.out.println(reverseNumber(num));
+		
+		num = 254790054;
+		System.out.println(num);
+		System.out.println(reverseNumber(num));
+		
+		System.out.println();
+		
+
+		char[] arr = "ABCDEF".toCharArray();
+		System.out.println(arr);
+		System.out.println(reverseCharArr(arr));
+
+		System.out.println();
+		arr = "ABCDEFG".toCharArray();
+		System.out.println(arr);
+		System.out.println(reverseCharArr(arr));
 	}
 }
