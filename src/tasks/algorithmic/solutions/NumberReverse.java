50b332c0f0410326a2e76c622bfd44828abe05e9
ASatklichov <asatklichov@inwk.com>
2019-12-13 13:52:47 +0100

    reversing number algorithms

--- src/tasks/algorithmic/solutions/NumberReverse.java
+++ src/tasks/algorithmic/solutions/NumberReverse.java
@@ -15,18 +15,17 @@ package tasks.algorithmic.solutions;
  */
 public class NumberReverse {
 
-	public int reverseNumber(int number) {
-
-		int reverse = 0;
-		while (number != 0) {
-			reverse = (reverse * 10) + (number % 10);
-			number = number / 10;
+	static int reverse(int n) {
+		int rev = 0;
+		while (n != 0) {
+			rev = rev * 10 + (n % 10);
+			n = n / 10;
 		}
-		return reverse;
+		return rev;
 	}
 
 	public static void main(String a[]) {
-		NumberReverse nr = new NumberReverse();
-		System.out.println("Result: " + nr.reverseNumber(17868));
+
+		System.out.println(reverse(143303));
 	}
 }
